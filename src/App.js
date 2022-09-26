05c55a6442522f090a9c1ee8d7860e3863893f1f
tomas oliver <tomi204@outlook.com>
2022-09-26 19:40:10 -0300

    navbar y proyecto inicio

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='./Logo.png'  alt='logo' className='logoI'/>
+     <li className='navbar'>
+      <a href="index.html">Home</a>
+      <a href="about.html">About Us</a>
+      <a href="contact.html">What We Offer</a>
+      <a href="contact.html">Projects</a>
+      <a href="contact.html">FAQ'S</a>
+</li>
+      </header>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
